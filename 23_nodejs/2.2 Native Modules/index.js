5984f39ca703a9676ef9b2ab5e316838304bbd2d
damian <cgsdamian@gmail.com>
2024-06-15 15:19:16 -0600

    adding section 23 nodejs

--- 23_nodejs/2.2 Native Modules/index.js
+++ 23_nodejs/2.2 Native Modules/index.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello world", (err) => {
+/*fs.writeFile("message.txt", "Hello world!", (err) => {
   if (err) throw err;
   console.log("The file has been saved");
+});*/
+
+fs.readFile("./message.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
